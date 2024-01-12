bf54ec92127a88b6f5c077f54dfa49addb9058c2
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-12 17:41:19 +0600

    "add"

--- main.go
+++ main.go
@@ -6,6 +6,4 @@ func main() {
 	var conferenceName = "GO Conference" // variable declaration
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName) // %v to print any variable
 	fmt.Println("Get your tickets here to attend")
-
-
 }
