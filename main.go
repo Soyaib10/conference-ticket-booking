f4e33cc7477e07db6502ae8a7d76519dc66c77da
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-12 18:24:59 +0600

    user input

--- main.go
+++ main.go
@@ -4,11 +4,31 @@ import "fmt"
 
 func main() {
 	// variable declaration
-	var conferenceName string = "GO Conference" 
+	conferenceName := "GO Conference" 
 	const conferenceTickets int = 50
-	remainingTickets := 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName) // %v to print any variable
 	fmt.Printf("We have total of %v tickets and %v are remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	// getting user input
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets;
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
