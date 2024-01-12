666043a4636edc0aea392da4a37f927afcffdd6a
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-12 17:40:01 +0600

    variables

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "GO Conference" // variable declaration
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName) // %v to print any variable
+	fmt.Println("Get your tickets here to attend")
+
+
+}
