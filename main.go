73075280f924f441c4ce39aeec4f79609bf584fa
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-12 18:05:57 +0600

    other ways of variable declaration

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	// variable declaration
-	var conferenceName = "GO Conference" 
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "GO Conference" 
+	const conferenceTickets int = 50
+	remainingTickets := 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName) // %v to print any variable
 	fmt.Printf("We have total of %v tickets and %v are remaining\n", conferenceTickets, remainingTickets)
