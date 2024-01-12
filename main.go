8b1e3d2f36be9f7d25b4113e6c80091759a55a5c
Soyaib10 <soyaibzihad10@gmail.com>
2024-01-12 22:22:31 +0600

    package level variable declaration to make code cleaner

--- main.go
+++ main.go
@@ -5,28 +5,31 @@ import (
 	"strings"
 )
 
+// package level variables: it does not support a := 5 type initialization. They are accessable from any function under the package they are. so the functions that uses these variables as parameter don't need this in parameter list. 
+const conferenceTickets int = 50
+
+var conferenceName = "GO Conference"
+var remainingTickets uint = 50
+var bookings []string
+
 func main() {
 	// variable declaration
-	conferenceName := "GO Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	var bookings []string
 
 	// greeting user function
-	greetingUsers(conferenceName, conferenceTickets, remainingTickets)
+	greetingUsers()
 
 	for {
 		// getting user input
 		firstName, lastName, email, userTickets := getUserInput()
 
 		// user input validation
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 			// book tickets
-			bookTicket(remainingTickets, userTickets, bookings, firstName, lastName, email, conferenceName)
+			bookTicket(userTickets, firstName, lastName, email)
 			// print first names
-			firstNames := getFirstNames(bookings)
+			firstNames := getFirstNames()
 			fmt.Printf("The first names of bookings are %v\n", firstNames)
 
 			// noTicketsRemaining := remainingTickets == 0 // can be written like this
@@ -48,13 +51,13 @@ func main() {
 	}
 }
 
-func greetingUsers(conferenceName string, conferenceTickets int, remainingTickets uint) {
+func greetingUsers() {
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName) // %v to print any variable
 	fmt.Printf("We have total of %v tickets and %v are remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
-func getFirstNames(bookings []string) []string { // function takes input and returns slice
+func getFirstNames() []string { // function takes input and returns slice
 	firstNames := []string{} // create an empty slice
 	for _, booking := range bookings {
 		var names = strings.Fields(booking) // Fields returns slices elements when whitespace found. names is a slice here
@@ -64,7 +67,7 @@ func getFirstNames(bookings []string) []string { // function takes input and ret
 	return firstNames
 }
 
-func validateUserInput(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) {
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
 	isValidEmail := strings.Contains(email, "@")
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
@@ -90,7 +93,7 @@ func getUserInput() (string, string, string, uint) {
 	return firstName, lastName, email, userTickets
 }
 
-func bookTicket(remainingTickets uint, userTickets uint, bookings []string, firstName string, lastName string, email string, conferenceName string) {
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets -= userTickets
 	bookings = append(bookings, firstName+" "+lastName)
 
